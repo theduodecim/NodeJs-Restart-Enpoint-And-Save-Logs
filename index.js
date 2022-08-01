5d12a4d5611e1e60049b8a169f4a4f27e05c7146
theduodecim <juansebastian.012@gmail.com>
2022-08-01 16:40:38 -0300

    changes

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+var fs = require('fs');
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+var log_stdout = process.stdout;
+var shell = require('shelljs');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
-});
+console.log = function(d) { //
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+
+
+const express = require('express')
+const app = express()
+
+
+app.get('/restart', (req,res)=>{
+  //restart or create a new instance of the server
+  shell.exec('forever restart /home/juan/Escritorio/test/webminTest/node1/node-hello/index.js');
+  console.log('restarting nodejs');
+  // then reply
+  res.json({
+      'message': 'server restarted successfully'
+     })
+  })
+
+app.get('/', (req, res) => {
+  console.log('connected');
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
